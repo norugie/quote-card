65fe73e2b0f013fb1c575958eb970af0fa7b374b
Rugie Ann Barrameda <norugie015@gmail.com>
2023-10-09 20:56:11 -0700

    added functionality for quote refresh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 import { useCallback, useEffect, useState } from 'react';
 
-const styles = ['orange-peel', 'notepaper', 'gradient', 'cursive-bubble', 'typewriter'];
+const styles = [
+    'orange-peel',
+    'notepaper',
+    'gradient',
+    'cursive-bubble',
+    'typewriter'
+];
 
 function App() {
     const [quote, setQuote] = useState("");
@@ -35,6 +41,7 @@ function App() {
 
     useEffect(function() {
         getQuote();
+        console.log('i fire once');
     }, [getQuote]);
 
     return (
@@ -42,10 +49,15 @@ function App() {
             <div className="main">
                 <span className="left">❝</span>
                 <QuoteMessage quote={quote} />
+                {/* <div className="quote">Etiam dictum finibus ligula, quis vulputate elit sodales porttitor. In fringilla laoreet faucibus. Fusce in diam ut magna eleifend iaculis in posuere eros.</div> */}
                 <span className="right">❞</span>
                 <QuoteAuthor author={author} />
+                {/* <div className="author">John Smith</div> */}
             </div>
-            <button className="app-button" onClick={getQuoteAndCount }>Get quote!</button>
+            <div className="btn-group">
+                <button className="btn-reload" onClick={getQuoteAndCount}>&#x21bb; Get new quote!</button>
+                <button className="btn-save">&#x1F4BE; Save</button>
+            </div>
             <QuoteCount count={count} />
         </div>
     );
@@ -56,13 +68,11 @@ function QuoteMessage({quote}) {
 }
 
 function QuoteAuthor({author}) {
-    return <div className="author">{author}</div>
+    return <div className="author">{author}</div>;
 }
 
 function QuoteCount({count}) {
-    return (
-        <p>You have read <strong>{count}</strong> piece(s) of quote.</p>
-    );
+    return <p style={{paddingTop: '20px', textAlign: 'center'}}>You have read <strong>{count}</strong> piece(s) of quotes.</p>;
 }
 
 export default App;
